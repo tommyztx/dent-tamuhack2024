9d99416cc77d3a24578ab27f095cceb7f5450fd7
Tommy Zheng <tommyztx@gmail.com>
2024-01-27 20:08:22 -0600

    new-config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_a6pDHb1z3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3n26135b5r5kbvhbou5pfu0utj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
